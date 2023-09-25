3c487101f7b88c888b2fac80a7db9636070addfd
Lupan <lucian.lupan@isa.utm.md>
2023-09-25 12:17:48 +0300

    Added readme.md for lab two(as a test commit)

---
+++ src/secondlab/Student.java
@@ -0,0 +1,12 @@
+package secondlab;
+
+import java.util.Scanner;
+
+public class Student {
+    int test;
+
+
+    public static void main(String[] args) {
+        System.out.println("hi");
+    }
+}
