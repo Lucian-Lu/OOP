837bc50dbb55094fd349a4b3a9ce2236b5d09ab6
Lupan <lucian.lupan@isa.utm.md>
2023-10-02 21:08:33 +0300

    Update Date.java to return a String format

--- src/secondlab/Date.java
+++ src/secondlab/Date.java
@@ -1,7 +1,33 @@
 package secondlab;
 
+import java.time.LocalDate;
+
 public class Date {
     private byte day;
     private byte month;
     private short year;
+
+    public Date(byte day, byte month, short year) {
+        this.day = day;
+        this.month = month;
+        this.year = year;
+    }
+
+    public byte getDay() {
+        return day;
+    }
+
+    public byte getMonth() {
+        return month;
+    }
+
+    public short getYear() {
+        return year;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%02d-%02d-%04d", day, month, year);
+    }
+
 }
