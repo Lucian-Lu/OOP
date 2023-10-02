4520bd331e5cf5a7deb1068148801130b5460c09
Lupan <lucian.lupan@isa.utm.md>
2023-10-02 08:35:07 +0300

    Add Faculty.java

---
+++ src/secondlab/Faculty.java
@@ -0,0 +1,16 @@
+package secondlab;
+
+import java.util.List;
+
+enum StudyField {
+    MECHANICAL_ENGINEERING, SOFTWARE_ENGINEERING, FOOD_TECHNOLOGY, URBANISM_ARCHITECTURE,
+    VETERINARY_MEDICINE
+}
+
+public class Faculty {
+    private String name;
+    private String abbreviation;
+    private List<Student> students;
+    private StudyField studyField;
+
+}
