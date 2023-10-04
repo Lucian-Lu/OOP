32c293cd66bbc31ab4c10da05285fc7c1b739b68
Lupan <lucian.lupan@isa.utm.md>
2023-10-04 23:22:13 +0300

    Update Main.java

--- src/secondlab/Main.java
+++ src/secondlab/Main.java
@@ -12,11 +12,3 @@ public class Main {
         app.closeScanner();
     }
 }
-
-// nf/Computers, Informatics & Microelectronics/isa/SOFTWARE_ENGINEERING
-// nf/Mechanics, Transport Engineering/mte/MECHANICAL_ENGINEERING
-// df/SOFTWARE_ENGINEERING
-// ns/isa/Lucian/Lupan/lucian.lupan@isa.utm.md/28/10/2002
-// ns/isa/Mihail/Mihalachi/mihail.mihalachi@isa.utm.md/16/11/2003
-// ns/isa/Catalin/Latcovschi/catalin.latcovschi@isa.utm.md/26/10/2003
-// ns/mte/Mike/Ross/mike.ross@mte.utm.md/26/10/1990
