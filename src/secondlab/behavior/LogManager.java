564c1be99856f066445e290b6e4f27cd25ebb8af
Lupan <lucian.lupan@isa.utm.md>
2023-10-04 21:25:09 +0300

    Add log management system

--- src/secondlab/behavior/LogManager.java
+++ src/secondlab/behavior/LogManager.java
@@ -1,2 +1,33 @@
-package secondlab.behavior;public class LogManager {
+package secondlab.behavior;
+
+import java.io.FileWriter;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class LogManager {
+
+    public static void clearLogFile() {
+        FileManager fileManager = new FileManager();
+        fileManager.setFileName(".\\src\\secondlab\\behavior\\saves\\logs.txt");
+
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(fileManager.getFileName()))) {
+        } catch (IOException e) {
+            System.out.println("Error while clearing logs.\n");
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void log (String logMessage) {
+        FileManager fileManager = new FileManager();
+        fileManager.setFileName(".\\src\\secondlab\\behavior\\saves\\logs.txt");
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(fileManager.getFileName(), true))){
+            String logTime = LocalDateTime.now().format(DateTimeFormatter.ofPattern("ss mm HH dd MM yyyy"));
+            writer.write(logTime + " | " + logMessage + "\n");
+        } catch (IOException e){
+            System.out.println("Error while creating logs.\n");
+            throw new RuntimeException(e);
+        }
+    }
 }
