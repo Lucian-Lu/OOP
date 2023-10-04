8bbbf21b2b09b5a807a4a0e724b4465c56a02d3e
Lupan <lucian.lupan@isa.utm.md>
2023-10-04 21:26:37 +0300

    Update Faculty.java with logs

--- src/secondlab/models/Faculty.java
+++ src/secondlab/models/Faculty.java
@@ -1,5 +1,7 @@
 package secondlab.models;
 
+import secondlab.behavior.LogManager;
+
 import java.util.*;
 
 
@@ -10,11 +12,11 @@ public class Faculty {
     private final StudyField studyField;
 
 
-    public Faculty(String name, String abbreviation, String studyField) {
+    public Faculty(String name, String abbreviation, StudyField studyField) {
         this.name = name;
         this.abbreviation = abbreviation;
         this.students = new ArrayList<>();
-        this.studyField = StudyField.valueOf(studyField);
+        this.studyField = studyField;
     }
 
     public String getName() {
@@ -33,8 +35,9 @@ public class Faculty {
         return students;
     }
 
-    public static void addStudentToFaculty(String abbreviation, String firstName, String lastName,
-                                    String email, Date enrollmentDate, Date birthDate) {
+    public static void createAndAddStudentToFaculty(String abbreviation, String firstName,
+                                                    String lastName, String email, Date enrollmentDate,
+                                                    Date birthDate) {
         University university = new University();
         for (Faculty faculty : university.getFaculties()) {
             if (faculty.getAbbreviation().equals(abbreviation)) {
@@ -42,10 +45,12 @@ public class Faculty {
                         email, enrollmentDate, birthDate);
                 faculty.students.add(student);
                 System.out.println("Student successfully added to faculty.\n");
+                LogManager.log("AUDIT: Student added to faculty");
                 return;
             }
         }
         System.out.println("Faculty not found.\n");
+        LogManager.log("WARN: Faculty not found while adding student");
     }
 
     public static void graduateStudent(String email) {
@@ -55,10 +60,13 @@ public class Faculty {
                 if (student.getEmail().equals(email)) {
                     student.setGraduate(true);
                     System.out.println("Student graduated successfully.\n");
+                    LogManager.log("AUDIT: Student (" + student.getEmail() +
+                            ") graduated");
                     return;
                 }
             }
         }
+        LogManager.log("WARN: Student not found while graduating student");
         System.out.println("Student not found. Please make sure to check for typos.\n");
     }
 
@@ -82,7 +90,11 @@ public class Faculty {
         }
         if (i == 1) {
             System.out.println("No students have been found. Check your faculty abbreviation.\n");
+            LogManager.log("WARN: Enrolled students not found for faculty - " +
+                    abbreviation);
+            return;
         }
+        LogManager.log("AUDIT: Displayed enrolled students for faculty - " + abbreviation);
     }
 
     public static void displayGraduatedStudents(String abbreviation) {
@@ -105,16 +117,29 @@ public class Faculty {
         }
         if (i == 1) {
             System.out.println("No students have been found. Check your faculty abbreviation.\n");
+            LogManager.log("WARN: Graduated students not found for faculty - " +
+                    abbreviation);
+            return;
         }
+        LogManager.log("AUDIT: Displayed graduated students for faculty - " + abbreviation);
     }
 
     public static void enrolledInFaculty(String abbreviation, String email) {
         String[] email_abbreviation = email.split("[@.]");
         if (abbreviation.equals(email_abbreviation[2])) {
             System.out.println("Student is enrolled in this faculty.\n");
+            LogManager.log("AUDIT: Found student (" + email +
+                    ") enrolled in " + abbreviation);
             return;
         }
         System.out.println("Student is not enrolled in this faculty.\n");
+        LogManager.log("WARN: Student (" + email +
+                ") not enrolled in " + abbreviation);
+    }
+
+    @Override
+    public String toString() {
+        return (name + "\n" + abbreviation + "\n" + studyField + "\n" + students + "\n");
     }
 
 }
