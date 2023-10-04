46750847792951977eba7c927f06f9427ad69f10
Lupan <lucian.lupan@isa.utm.md>
2023-10-04 13:32:08 +0300

    Update Faculty.java with faculty functions.

--- src/secondlab/models/Faculty.java
+++ src/secondlab/models/Faculty.java
@@ -1,94 +1,120 @@
 package secondlab.models;
 
-import secondlab.Date;
-
 import java.util.*;
-import java.time.LocalDate;
 
 
 public class Faculty {
-    private String name;
-    private String abbreviation;
+    private final String name;
+    private final String abbreviation;
     private List<Student> students;
-    private StudyField studyField;
-    private secondlab.Date gradDate;
+    private final StudyField studyField;
 
 
-    public Faculty() {
+    public Faculty(String name, String abbreviation, String studyField) {
+        this.name = name;
+        this.abbreviation = abbreviation;
         this.students = new ArrayList<>();
+        this.studyField = StudyField.valueOf(studyField);
     }
 
-    // TODO change name to reflect the method's functionality better
-    public void assignFaculty() {
-        // TODO not assign but assign student to faculty
-
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter the faculty name: ");
-        this.name = scanner.next();
-        System.out.print("Enter faculty abbreviation: ");
-        this.abbreviation = scanner.next();
-        System.out.println("Enter student details: ");
-        Student student = new Student();
-        student.createStudent();
-        this.students.add(student);
-        System.out.println("Pick from the study fields:\n" +
-                "    1 = MECHANICAL_ENGINEERING,\n" +
-                "    2 = SOFTWARE_ENGINEERING,\n" +
-                "    3 = FOOD_TECHNOLOGY,\n" +
-                "    4 = URBANISM_ARCHITECTURE,\n" +
-                "    5 = VETERINARY_MEDICINE");
-        byte option;
-        option = scanner.nextByte();
-        switch (option){
-            case 1:
-                this.studyField = StudyField.MECHANICAL_ENGINEERING;
-                break;
-            case 2:
-                this.studyField = StudyField.SOFTWARE_ENGINEERING;
-                break;
-            case 3:
-                this.studyField = StudyField.FOOD_TECHNOLOGY;
-                break;
-            case 4:
-                this.studyField = StudyField.URBANISM_ARCHITECTURE;
-                break;
-            case 5:
-                this.studyField = StudyField.VETERINARY_MEDICINE;
-                break;
-            default:
-                System.out.println("Invalid option. Try again.");
-        }
-
-        System.out.println("Faculty Name = " + this.name);
-        System.out.println("Faculty Abbreviation = " + this.abbreviation);
-        System.out.println("Faculty Students? = " + this.students);
-        System.out.println("Study Field = " + this.studyField);
-
-        //scanner.close();
+    public String getName() {
+        return name;
     }
 
-    public void setGraduated() {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Input student email to modify the graduation status of: ");
-        String email = scanner.next();
-        for (Student student : students) {
-            if (student.getEmail().equals(email)) {
-                System.out.print("Input the student graduation status of the student: ");
-                boolean gradStatus = scanner.nextBoolean();
-                student.setGraduatedStatus(gradStatus);
+    public String getAbbreviation() {
+        return abbreviation;
+    }
+
+    public StudyField getStudyField() {
+        return studyField;
+    }
+
+    public List<Student> getStudents() {
+        return students;
+    }
+
+    public static void addStudentToFaculty(String abbreviation, String firstName, String lastName,
+                                    String email, Date enrollmentDate, Date birthDate) {
+        University university = new University();
+        for (Faculty faculty : university.getFaculties()) {
+            if (faculty.getAbbreviation().equals(abbreviation)) {
+                Student student = new Student(firstName, lastName,
+                        email, enrollmentDate, birthDate);
+                faculty.students.add(student);
+                System.out.println("Student successfully added to faculty.\n");
+                return;
             }
         }
-        System.out.print(students);
-        scanner.close();
-//        LocalDate date = LocalDate.now();
-//        String dateString = date.toString();
-//        String[] dateString2 = dateString.split("-");
-//        ArrayList<Integer> dateSt = new ArrayList<>();
-//        for (String test : dateString2) {
-//            int test2 = Integer.parseInt(test);
-//            dateSt.add(test2);
-//        }
-//        Collections.swap(dateSt, 0, 2);
+        System.out.println("Faculty not found.\n");
+    }
+
+    public static void graduateStudent(String email) {
+        University university = new University();
+        for (Faculty faculty : university.getFaculties()) {
+            for (Student student : faculty.getStudents()) {
+                if (student.getEmail().equals(email)) {
+                    student.setGraduate(true);
+                    System.out.println("Student graduated successfully.\n");
+                    return;
+                }
+            }
+        }
+        System.out.println("Student not found. Please make sure to check for typos.\n");
+    }
+
+    public static void displayEnrolledStudents(String abbreviation) {
+        University university = new University();
+        int i = 1;
+        for (Faculty faculty : university.getFaculties()) {
+            if (faculty.getAbbreviation().equals(abbreviation)) {
+                for (Student student : faculty.getStudents()) {
+                    if (!student.getGraduate()) {
+                        System.out.println("Student nr: " + i + "\n" +
+                                "Student first name: " + student.getFirstName() + "\n" +
+                                "Student last name: " + student.getLastName() + "\n" +
+                                "Student email: " + student.getEmail() + "\n" +
+                                "Student enrollment date: " + student.getEnrollmentDate() + "\n" +
+                                "Student date of birth: " + student.getDateOfBirth() + "\n");
+                        i++;
+                    }
+                }
+            }
+        }
+        if (i == 1) {
+            System.out.println("No students have been found. Check your faculty abbreviation.\n");
+        }
+    }
+
+    public static void displayGraduatedStudents(String abbreviation) {
+        University university = new University();
+        int i = 1;
+        for (Faculty faculty : university.getFaculties()) {
+            if (faculty.getAbbreviation().equals(abbreviation)) {
+                for (Student student : faculty.getStudents()) {
+                    if (student.getGraduate()) {
+                        System.out.println("Student nr: " + i + "\n" +
+                                "Student first name: " + student.getFirstName() + "\n" +
+                                "Student last name: " + student.getLastName() + "\n" +
+                                "Student email: " + student.getEmail() + "\n" +
+                                "Student enrollment date: " + student.getEnrollmentDate() + "\n" +
+                                "Student date of birth: " + student.getDateOfBirth() + "\n");
+                        i++;
+                    }
+                }
+            }
+        }
+        if (i == 1) {
+            System.out.println("No students have been found. Check your faculty abbreviation.\n");
+        }
+    }
+
+    public static void enrolledInFaculty(String abbreviation, String email) {
+        String[] email_abbreviation = email.split("[@.]");
+        if (abbreviation.equals(email_abbreviation[2])) {
+            System.out.println("Student is enrolled in this faculty.\n");
+            return;
+        }
+        System.out.println("Student is not enrolled in this faculty.\n");
     }
 
 }
