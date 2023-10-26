57cc7ee317d40a37d58938e043de6d8de97a2c27
Lupan <lucian.lupan@isa.utm.md>
2023-10-26 21:50:14 +0300

    Update ImageFile.java with basic functions

--- src/thirdlab/models/ImageFile.java
+++ src/thirdlab/models/ImageFile.java
@@ -1,7 +1,26 @@
 package thirdlab.models;
 
-public class ImageFile extends File {
-    public void checkExtension() {
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
+public class ImageFile {
+    public void getImageSize(String imagePath) {
+        try {
+            File image = new File(imagePath);
+            BufferedImage bufferedImage = ImageIO.read(image);
+            if (bufferedImage != null) {
+                int width = bufferedImage.getWidth();
+                int height = bufferedImage.getHeight();
+                System.out.println("Image width = " + width);
+                System.out.println("Image height = " + height);
+            } else {
+                System.out.println("Image could not be read.");
+            }
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
