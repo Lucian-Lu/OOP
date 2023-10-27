e5e091e73bc84f3ac4a422601fe11302a216c59c
Lupan <lucian.lupan@isa.utm.md>
2023-10-27 23:56:59 +0300

    Update Main.java with directory detection

--- src/thirdlab/Main.java
+++ src/thirdlab/Main.java
@@ -1,11 +1,29 @@
-package thirdlab.behavior;
+package thirdlab;
+
+import thirdlab.behavior.ApplicationLoop;
+import thirdlab.behavior.DirectoryChangeWatcher;
+import thirdlab.models.DefaultFile;
 
 public class Main {
     public static void main(String[] args) {
         ApplicationLoop app = new ApplicationLoop();
-        //FileChangeDetector.startFileChangeDetection(app);
-        app.run();
+
+        Thread appThread = new Thread(app::run);
+        appThread.start();
+
+        Thread watcherThread = new Thread(() -> {
+            String directoryPath = DefaultFile.getFolderLocation();
+            DirectoryChangeWatcher.watchDirectory(directoryPath);
+        });
+        watcherThread.start();
+
+        try {
+            appThread.join();
+            watcherThread.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
         app.closeScanner();
-        //FileChangeDetector.stopFileChangeDetection();
     }
 }
