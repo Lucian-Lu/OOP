a76bd40ad794d24bcede146257a21dff7f44eec3
Lupan <lucian.lupan@isa.utm.md>
2023-10-27 16:32:58 +0300

    Add basic functions to ProgramFile.java

--- src/thirdlab/models/ProgramFile.java
+++ src/thirdlab/models/ProgramFile.java
@@ -1,4 +1,70 @@
 package thirdlab.models;
 
-public class ProgramFile extends File {
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class ProgramFile {
+
+    public void analyzeJavaFile(String filePath) {
+        int lineCount = 0;
+        int classCount = 0;
+        int methodCount = 0;
+
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(filePath));
+            String line;
+
+            while ((line = reader.readLine()) != null) {
+                line = line.trim();
+                lineCount++;
+                if (line.matches("\\s*class.*") ||
+                        line.matches(".*(public|private|protected)\\s+class.*")){
+                    classCount++;
+                } else if (line.matches(".*(public|private|protected).*\\(.*\\)\\s*\\{.*")) {
+                    methodCount++;
+                }
+            }
+
+            reader.close();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Total lines: " + lineCount);
+        System.out.println("Total classes: " + classCount);
+        System.out.println("Total methods: " + methodCount);
+    }
+
+    public void analyzePythonFile(String filePath) {
+        int lineCount = 0;
+        int classCount = 0;
+        int methodCount = 0;
+
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(filePath));
+            String line;
+
+            while ((line = reader.readLine()) != null) {
+                line = line.trim();
+                lineCount++;
+
+                if (line.matches("\\s*class.*")) {
+                    classCount++;
+                } else if (line.matches("\\s*def.*\\(.*\\):")) {
+                    methodCount++;
+                }
+            }
+
+            reader.close();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Total lines: " + lineCount);
+        System.out.println("Total classes: " + classCount);
+        System.out.println("Total methods: " + methodCount);
+    }
 }
