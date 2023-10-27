56181b9e95b808981956b63e6d5066e8408759d5
Lupan <lucian.lupan@isa.utm.md>
2023-10-27 23:56:01 +0300

    Move saves to thirdlab package

--- src/thirdlab/Main.java
+++ src/thirdlab/Main.java
@@ -3,6 +3,9 @@ package thirdlab.behavior;
 public class Main {
     public static void main(String[] args) {
         ApplicationLoop app = new ApplicationLoop();
+        //FileChangeDetector.startFileChangeDetection(app);
         app.run();
+        app.closeScanner();
+        //FileChangeDetector.stopFileChangeDetection();
     }
 }
